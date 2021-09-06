56d445f5c208828c930989e7756c19147ff8342d
Devin Abbott <devinabbott@gmail.com>
2021-09-06 12:05:24 -0700

    Update home

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,113 +38,6 @@ export async function fetchAllTokenIds(
 }
 
 export default function Home() {
-  const ethereum = typeof window !== "undefined" && (window as any).ethereum;
-  const [enabled, setEnabled] = useState(true);
-  const [isMinting, setIsMinting] = useState(false);
-  const [tokenUri, setTokenUri] = useState<string | undefined>(undefined);
-  const [status, setStatus] = useState<ReactNode>(
-    ethereum ? (
-      "Click the button to mint. It's free (minus gas)."
-    ) : (
-      <>
-        {`Your browser doesn't support web3. Consider installing `}
-        <a href="https://metamask.io/">MetaMask</a>.
-      </>
-    )
-  );
-
-  const mint = useCallback(() => {
-    async function mint() {
-      setStatus("Requesting permission...");
-
-      await ethereum.enable();
-
-      const provider = new ethers.providers.Web3Provider(ethereum);
-
-      const signer = provider.getSigner();
-
-      const contract = new ethers.Contract(contractAddress, abi, provider);
-
-      const address = await signer.getAddress();
-
-      const contractWithSigner = contract.connect(signer);
-
-      const transaction = await contractWithSigner.claim();
-
-      setIsMinting(true);
-
-      setStatus("Minting... this usually takes about 30 seconds.");
-
-      await transaction.wait();
-
-      setIsMinting(false);
-
-      setStatus("Minted.");
-
-      const genericOpenSeaLink = (
-        <div>
-          Minted. Your NFT will appear on{" "}
-          <a href="https://opensea.io/collection/openpalette">OpeaSea</a>{" "}
-          shortly.
-        </div>
-      );
-
-      try {
-        const tokenIds = await fetchAllTokenIds(contract, address);
-
-        if (tokenIds.length === 0) {
-          setStatus(genericOpenSeaLink);
-          return;
-        }
-
-        tokenIds.reverse();
-
-        const [newest] = tokenIds;
-
-        console.log(tokenIds, newest);
-
-        const tokenUri = await contract.tokenURI(newest);
-
-        setTokenUri(tokenUri);
-        setStatus(genericOpenSeaLink);
-      } catch (e) {
-        console.log("Mint succeeded, but failed to fetch token ids");
-        console.log(e);
-
-        setStatus(
-          <div>
-            Minted. Your NFT will appear on{" "}
-            <a href="https://opensea.io/collection/openpalette">OpeaSea</a>{" "}
-            shortly.
-          </div>
-        );
-      }
-    }
-
-    setEnabled(false);
-
-    mint()
-      .catch((e) => {
-        console.error(e);
-
-        setStatus("Aborted.");
-
-        setEnabled(true);
-      })
-      .then(() => {
-        setIsMinting(false);
-      });
-  }, [ethereum]);
-
-  const svgString = useMemo(() => {
-    if (!tokenUri) return;
-
-    const tokenData = decodeTokenData(tokenUri);
-    const imageData = tokenData.image;
-
-    return imageData;
-  }, [tokenUri]);
-
   return (
     <div
       className="container"
@@ -160,34 +53,22 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {svgString ? (
-        <div
-          style={{
-            width: 500,
-            height: 500,
-            backgroundSize: "cover",
-            backgroundImage: `url('${svgString}')`,
-          }}
-        />
-      ) : (
-        <>
-          <img
-            style={{
-              width: 300,
-              height: 300,
-              marginBottom: "4rem",
-              filter: "drop-shadow(0px 2px 4px black)",
-            }}
-            src="/logo.png"
-            alt=""
-          />
-          <button onClick={mint} disabled={!enabled}>
-            Mint my <strong>OpenPalette</strong>
-          </button>
-        </>
-      )}
-      <span className={"status " + (isMinting ? "flickerAnimation" : "")}>
-        {status}
+
+      <img
+        style={{
+          width: 300,
+          height: 300,
+          marginBottom: "4rem",
+          filter: "drop-shadow(0px 2px 4px black)",
+        }}
+        src="/logo.png"
+        alt=""
+      />
+      <button onClick={() => {}} disabled={true}>
+        Sold out.
+      </button>
+      <span className={"status flickerAnimation"}>
+        Thank you for the support! 🌈
       </span>
     </div>
   );
